b35003f1e2ce3ff5994f4f3087b968c3a131e7d5
Michael Ilg <michael@ilg.ac>
2016-12-27 15:56:15 -0800

    Implement Vue router

--- public/src/App.vue
+++ public/src/App.vue
@@ -16,6 +16,19 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <div id="app2">
+  <h1>Hello App moin moin!</h1>
+  <p>
+    <!-- use router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- <router-link> will be rendered as an `<a>` tag by default -->
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view></router-view>
+</div>
   </div>
 </template>
 
--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 import App from './App.vue'
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+const router = new VueRouter({
+  routes // short for routes: routes
+})
+
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
