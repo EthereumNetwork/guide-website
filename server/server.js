aabdcc04a2a07c9920047e9b0ca55e936e438afa
Michael Ilg <michael@ilg.ac>
2016-12-27 16:54:10 -0800

    Setup Express

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var history = require('connect-history-api-fallback');
+// var db = require('./db.js') deactivate for now due to limited AWS storage space :/
+
+var app = express();
+
+// server-side routes
+app.get('/api/dapps', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+// serving index.html and client-side routes with Vue router
+app.use(history())
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Server started and');
+});
