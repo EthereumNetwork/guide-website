3f4ff0956879dc4b4cbe4b78a3c7cfc094a805d3
dharm_tracker <dharmandarjob@gmail.com>
2017-01-03 17:23:51 +0530

    dapp listing design

--- public/src/components/DappItem.vue
+++ public/src/components/DappItem.vue
@@ -1,8 +1,18 @@
 <template>
-  <li>
-    <h5>{{dapp.title}}</h5>
-    <p>{{dapp.description}}</p>
-  </li>
+<v-list-item>
+    <v-list-item-avatar class="dapp-listitem-color" large>description</v-list-item-avatar>
+    <v-list-item-title>
+      <span>{{dapp.title}}</span>
+      <v-list-item-sub-title>{{dapp.description}}</v-list-item-sub-title>
+    </v-list-item-title>
+<!--    <v-list-item-action>
+      <v-list-item-action-title>...</v-list-item-action-title>
+      <a href="#!">
+        <v-icon class="grey--text darken-1">email</v-icon>
+    </v-list-item-action>-->
+</v-list-item>
+
+
 </template>
 
 <script>
@@ -16,3 +26,9 @@ export default {
   }
 }
 </script>
+<style>
+.dapp-item-color
+{
+color:red;
+}
+</style>
