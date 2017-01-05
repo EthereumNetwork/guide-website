62c3367c71a11e4d5adfe5211eeb5060d6fccd5b
dharm_tracker <dharmandarjob@gmail.com>
2017-01-05 23:50:19 +0530

    merging code

---
+++ public/src/Twitter.Vue
@@ -0,0 +1,18 @@
+<template>
+<v-container fluid>
+  <v-row>
+    <v-col xs12 sm6 md4 lg12 >
+      <!--<a href="https://twitter.com/EthereumPress" class="twitter-follow-button" data-show-count="false">Follow @EthereumPress</a>-->
+    </v-col>
+    <v-col xs12 sm6 md4 lg12>
+      <a class="twitter-timeline"  href="https://twitter.com/EthereumPress">Tweets by Ethereum Press</a>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+
+
+
+</script>
