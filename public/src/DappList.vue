02e9d5d7253af5f5d131b0481326ce2ed1545ab9
Michael Ilg <michael@ilg.ac>
2017-01-09 07:35:36 -0800

    Clean up DappList and db

--- public/src/DappList.vue
+++ public/src/DappList.vue
@@ -1,104 +1,70 @@
 <template>
-<v-container fluid>
-<!--<div>
-      <p>until now the dapp list is just an array on the client side, I want to connect a MongoDB sever-side and fill it up...</p>
-      <p>use the search bar to filter dapps</p>
-    </div>-->
-
-<v-row>
-    <v-col lg12>
-        <v-list>
-          <dapp-item v-for="dapp in filteredDapps" v-bind:dapp="dapp"></dapp-item>
-        </v-list>
-    </v-col>
+  <v-container fluid>
+    <v-row>
+      <v-col lg12>
+          <v-list>
+            <dapp-item v-for="dapp in filteredDapps" v-bind:dapp="dapp"></dapp-item>
+          </v-list>
+      </v-col>
+    </v-row>
     <router-link to="/Form">Add new Dapp</router-link>
-</v-row>
-</v-container>
+  </v-container>
 </template>
 
-
-
 <script>
+  import DappItem from './components/DappItem.vue'
 
-import DappItem from './components/DappItem.vue'
-function demo()
-{
-var dappList=[];//[{ title: 'Title 1', description: 'Golem is great' },{ title: 'Augur', description: 'Augur is a prediction market'  },{ title: 'Whatever else Ethereum can do', description: 'Ethereum is the new kid on the block'  }];
-
-$.ajax({
+  export default {
+    name: 'app',
+    props: ['searchField'],
+    data () {
+      return {
+        dappList: [{title:'No dapp data found',description:'please refresh the page to get the newest dapp data'}]
+      }
+    },
+    beforeCreate () {
+      var that = this;
+      $.ajax({
         url: serverUrl+'/api/dapps',
         type: 'Get',
-
-        beforeSend: function (request) {
-        },
         success: function(data) {
-        var i=0;
-        for(i=0;i<data.length;i++)
-        {
-          var d=data[i];
-          var isShowContact=true;
-          if(d.contact.github=='' && d.contact.website=='' && d.contact.slack==''&& d.contact.reddit=='' && d.contact.twitter=='')
-          {
-          isShowContact=false;
-
-          }else{
-            isShowContact=true;
+          if (data) {
+            console.log('data received: ', data);
+            that.dappList = data;
           }
-          dappList.push({title:d.title,description:d.description,github:d.contact.github,website:d.contact.website,slack:d.contact.slack,reddit:d.contact.reddit,twitter:d.contact.twitter,IsShowContact:isShowContact});
-        }
-        if(i==0)
-        {
-          dappList.push({title:'No dapp data found',description:'description part not implemented yet'});
-        }
-
         }
       });
+    },
+    computed: {
+      filteredDapps: function () {
+          var dappList_array = this.dappList,
+              searchField = this.searchField;
 
+          if(!searchField){
+              return dappList_array;
+          }
 
-return dappList;
-}
+          searchField = searchField.trim().toLowerCase();
 
-export default {
-  name: 'app',
-  props: ['searchField'],
-  data () {
-    return {
+          dappList_array = dappList_array.filter(function(item){
+              if(item.description.toLowerCase().indexOf(searchField) !== -1){
+                  return item;
+              }
+          })
 
-      dappList: demo()
+          // Return an array with the filtered data.
+          return dappList_array;;
+      }
+    },
+    components: {
+      DappItem
     }
-  },
-  computed: {
-    // A computed property that holds only those articles that match the searchField.
-    filteredDapps: function () {
-        var dappList_array = this.dappList,
-            searchField = this.searchField;
-
-        if(!searchField){
-            return dappList_array;
-        }
-
-        searchField = searchField.trim().toLowerCase();
-
-        dappList_array = dappList_array.filter(function(item){
-            if(item.description.toLowerCase().indexOf(searchField) !== -1){
-                return item;
-            }
-        })
-
-        // Return an array with the filtered data.
-        return dappList_array;;
-    }
-  },
-  components: {
-    DappItem
   }
-}
 </script>
 
 <style>
-.dapp-listitem-color
-{
+  .dapp-listitem-color {
     background-color: #b59f9f!important;
     border-color: #b59f9f!important;
-}
+  }
 </style>
--- server/db.js
+++ server/db.js
@@ -2,15 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/EthNW');
 mongoose.Promise = Promise;
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log('and we are connected to MongoDB with following dapps:');
-// });
-
 var Schema = mongoose.Schema;
-
-
 var dappSchema = new Schema({
   title: String,
   owner: String,
@@ -26,15 +18,8 @@ var dappSchema = new Schema({
     reddit: String,
   }
 });
+
 var Dapp = mongoose.model('Dapp', dappSchema);
-
-// var first = new Dapp({ title: 'Augur' });
-// console.log(first); // 'Silence'
-// first.save(function (err, first) {
-//   if (err) return console.error(err);
-//   console.log(first.title, 'created');
-// });
-
 Dapp.find().then(dapps => console.log('following dapps found:', dapps));
 
 module.exports.Dapp = Dapp;
