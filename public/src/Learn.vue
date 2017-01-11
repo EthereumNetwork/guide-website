75bbf5bd71f75dc7540943334c8d96bc0217617f
Michael Ilg <michael@ilg.ac>
2017-01-10 22:32:01 -0800

    Add sources

--- public/src/Learn.vue
+++ public/src/Learn.vue
@@ -1,15 +1,22 @@
 <template>
 <div>
-  <h2>What is Ethereum?</h2>
+  <h2>Ethereum</h2>
+  <p>Ethereum is a decentralized platform that runs smart contracts: applications that run exactly as programmed without any possibility of downtime, censorship, fraud or third party interference.</p>
+  <p>These apps run on a custom built blockchain, an enormously powerful shared global infrastructure that can move value around and represent the ownership of property. This enables developers to create markets, store registries of debts or promises, move funds in accordance with instructions given long in the past (like a will or a futures contract) and many other things that have not been invented yet, all without a middle man or counterparty risk.</p>
+  <p>The project was bootstrapped via an ether pre-sale during August 2014 by fans all around the world. It is spearheaded by the Ethereum Foundation, a Swiss nonprofit, with contributions from great minds across the globe.</p>
+  <p>Below you'll find some links to get you started. So far it's just a collection of links, I'm in the process of curating and organizing them in a step-by-step manner</p>
+  <h4>What is Ethereum?</h4>
   <a href="http://blockgeeks.com/guides/what-is-ethereum-a-step-by-step-beginners-guide/" target="_blank" class="learnLink">What is Ethereum?</a> A Step-by-Step Beginners Guide<br>
-  <a href="https://youtu.be/A1Vbrxkqjwc" target="_blank" class="learnLink">New Kids on the Blockchain</a>What can you use it for?<br>
-  <p>...some more and better links needed here...</p>
+  <a href="https://youtu.be/A1Vbrxkqjwc" target="_blank" class="learnLink">New Kids on the Blockchain</a> What can you use it for?<br>
+  <a href="https://youtu.be/eRDKP8nCVtU" target="_blank" class="learnLink">Ethereum in 100 seconds</a> by Vitalik Buterin and Dr. Gavin Wood<br>
+  <a href="https://youtu.be/66SaEDzlmP4" target="_blank" class="learnLink">Ethereum in 25 minutes</a> by Vitalik Buterin<br>
+  <a href="https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g/videos" target="_blank" class="learnLink">Videos from Ethereum DevCon 2</a> Sep. 2016 in Shanghai<br>
   <br>
-  <h2>Developer Resources and Tools</h2>
+  <h4>Developer Resources and Tools</h4>
   <a href="https://ethereum.org/" target="_blank" class="learnLink">Ethereum Website</a> Browser and first tutorials from the Ethereum Foundation<br>
   <a href="https://github.com/ethereum/wiki/wiki/White-Paper" target="_blank" class="learnLink">White Paper</a> The formal proposal of Ethereum<br>
   <a href="https://github.com/ethereum/yellowpaper" target="_blank" class="learnLink">Yellow Paper</a> The formal specification of the Ethereum protocol<br>
-  <a href="https://solidity.readthedocs.io/en/latest/" target="_blank" class="learnLink">Solidity Docs</a><br>
+  <a href="https://solidity.readthedocs.io/en/latest/" target="_blank" class="learnLink">Solidity Docs</a> learn the contract-oriented, high-level language for the Ethereum Virtual Machine<br>
   <a href="http://ethereum.stackexchange.com" target="_blank" class="learnLink">Ethereum StackExchange</a> get answers for all your Ethereum questions<br>
   <a href="http://truffleframework.com/" target="_blank" class="learnLink">Truffe</a> Development framework for Ethereum<br>
   <a href="https://github.com/nexusdev/dapple" target="_blank" class="learnLink">Dapple</a> EVM contract system developer multitool<br>
@@ -17,13 +24,13 @@
   <a href="https://github.com/blockapps/bloc" target="_blank" class="learnLink">BlockApps</a> REST API and Javascript and Mobile SDK to Ethereum blockchains.<br>
   <a href="https://infura.io/" target="_blank" class="learnLink">Infura</a> APIs to Ethereum and IPFS nodes<br>
   <a href="https://ethereum.github.io/browser-solidity/" target="_blank" class="learnLink">Browser Solidity</a> Solidity Realtime compiler and runtime<br>
-  <h2>Wallets and Browsers</h2>
+  <h4>Wallets and Browsers</h4>
   <a href="https://metamask.io/" target="_blank" class="learnLink">MetaMask</a> Chrome plugin to access Ethereum dApps<br>
   <a href="https://github.com/ethereum/mist/releases" target="_blank" class="learnLink">Mist</a> Official Ethereum browser and wallet<br>
   <a href="https://status.im/" target="_blank" class="learnLink">Status</a> Ethereum browser/messenger/wallet for mobile phones<br>
   <a href="https://www.myetherwallet.com/" target="_blank" class="learnLink">MyEtherWallet</a> client-side Ethereum online wallet<br>
   <br>
-  <h2>Clients and Implementations</h2>
+  <h4>Clients and Implementations</h4>
   <a href="https://geth.ethereum.org/" target="_blank" class="learnLink">Geth Ethereum Client</a> Ethereum client developed in Go<br>
   <a href="https://ethcore.io/parity.html" target="_blank" class="learnLink">Parity Ethereum Client</a> Ethereum client developed in Rust<br>
   <a href="http://ethereumjs.github.io/" target="_blank" class="learnLink">ethereumJS</a> Javascript gateway to Ethereum<br>
@@ -32,32 +39,47 @@
   <a href="https://github.com/web3j/web3j" target="_blank" class="learnLink">web3j</a> Java and Android Ethereum library<br>
   <a href="https://github.com/ethereum/pyethereum" target="_blank" class="learnLink">pyethereum</a> Python core library of the Ethereum project<br>
   <br>
-  <h2>Guides and Tips:</h2>
-  <a href="https://medium.com/@ConsenSys/a-101-noob-intro-to-programming-smart-contracts-on-ethereum-695d15c1dab4#.tcmcvxugj" target="_blank" class="learnLink">101 Noob Intro</a> to Programming Smart Contracts<br>
-  <a href="http://blockgeeks.com/guides/smart-contracts-the-blockchain-technology-that-will-replace-lawyers/" target="_blank" class="learnLink">Smart Contracts</a> Beginner’s Guide<br>
-  <a href="https://medium.com/metamask/developing-ethereum-dapps-with-truffle-and-metamask-aa8ad7e363ba#.5yyusxbic" target="_blank" class="learnLink">Developing Ethereum Dapps</a> with Truffle and MetaMask<br>
-  <a href="https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g/videos" target="_blank" class="learnLink">Devcon2 videos</a> and <a href="https://ethereumfoundation.org/devcon/?page_id=14" target="_blank" class="learnLink">slides</a><br>
-  <a href="https://medium.com/@mvmurthy/ethereum-for-web-developers-890be23d1d0c" target="_blank" class="learnLink">Ethereum for web developers</a><br>
-  <a href="https://medium.com/@ConsenSys/very-deep-dive-on-ethereum-reading-list-f5b1122e5990#.d9mtt32ic" target="_blank" class="learnLink">Ethereum Reading List</a> for more in depth learning<br>
-  <a href="http://decypher.tv/series/ethereum-development" target="_blank" class="learnLink">Video Series</a> for Ethereum Ðapp Development (paid)<br>
-  <a href="https://academy.b9lab.com/" target="_blank" class="learnLink">Certified Online Course</a> for Ethereum Developer (paid)<br>
-  <a href="http://exosphe.re/#blockchain" target="_blank" class="learnLink">8 week immersive program</a> for Blockchain App Dev (paid)<br>
-  <br>
-  <h2>Security</h2>
+  <h4>Security</h4>
   <a href="https://medium.com/@pipermerriam/my-guide-to-solid-digital-security-fb76cb19c536#.cliexcgpg" target="_blank" class="learnLink">Guide to securing your digital life</a>absolutely necessary when dealing with cryptocurrencies<br>
+  <a href="https://www.ledgerwallet.com/products/12-ledger-nano-s" target="_blank" class="learnLink">Ledger Nano</a> Hardware wallet to store your ether and <a href="https://ledger.groovehq.com/knowledge_base/topics/how-to-secure-your-eth-tokens-augur-rep-dot-dot-dot-with-your-nano-s" target="_blank" class="learnLink">Tokens</a><br>
   <a href="https://download.slock.it/public/DAO/WhitePaper.pdf" target="_blank" class="learnLink">The infamous DAO</a> How a good idea went wrong<br>
   <a href="https://github.com/raineorshine/solgraph" target="_blank" class="learnLink">Solgraph</a> Visualize Solidity control flow for smart contract security analysis.<br>
   <a href="https://medium.com/zeppelin-blog/zeppelin-v1-0-0-4e593c505345#.bv6yubf7v" target="_blank" class="learnLink">Zeppelin</a> framework to build secure smart contracts in the Solidity language<br>
   <a href="https://monax.io/docs/tutorials/solidity/solidity_1_the_five_types_model/" target="_blank" class="learnLink">Solidity series on Eris</a> different blockchain, same language<br>
   <a href="http://doublenegative.com/ethereum-security-and-wallets/" target="_blank" class="learnLink">Wallet Security</a> and how to secure them<br>
   <br>
-  <h2>to be continued...</h2>
-  this is a random collection of links, still needs to get organized or tossed...
-  http://ethereum.stackexchange.com/questions/607/how-to-unit-test-smart-contracts
-  https://github.com/Great-Hill-Corporation/nameGame (see comments and issues:  'security' issues are in the use of 'send' or 'call'. A contract's programmer must assume that the addresses behind the destinations of a 'send' or 'call' are malicious. If there were no sends or calls in my contract, it seems most of the 'issues' would be simple to fix. Exposing the fact that 'every unknown address may be malicious' is a great take-away from this exercise.
-
-  http://blockchainers.org/index.php/2016/11/27/ethereum-usecase-claim-and-endorse/ A generic “Claim and Endorse” Contract
-
+  <h4>Guides and Tips:</h4>
+  <a href="https://medium.com/@ConsenSys/a-101-noob-intro-to-programming-smart-contracts-on-ethereum-695d15c1dab4#.tcmcvxugj" target="_blank" class="learnLink">101 Noob Intro</a> to Programming Smart Contracts<br>
+  <a href="http://blockgeeks.com/guides/smart-contracts-the-blockchain-technology-that-will-replace-lawyers/" target="_blank" class="learnLink">Smart Contracts</a> Beginner’s Guide<br>
+  <a href="https://medium.com/metamask/developing-ethereum-dapps-with-truffle-and-metamask-aa8ad7e363ba#.5yyusxbic" target="_blank" class="learnLink">Developing Ethereum Dapps</a> with Truffle and MetaMask<br>
+  <a href="https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g/videos" target="_blank" class="learnLink">Devcon2 videos</a> and <a href="https://ethereumfoundation.org/devcon/?page_id=14" target="_blank" class="learnLink">slides</a><br>
+  <a href="https://medium.com/@mvmurthy/ethereum-for-web-developers-890be23d1d0c" target="_blank" class="learnLink">Ethereum for web developers</a><br>
+  <a href="http://blockchainers.org/index.php/2016/11/27/ethereum-usecase-claim-and-endorse" target="_blank" class="learnLink"> A generic “Claim and Endorse” Contract</a><br>
+  <a href="https://medium.com/@ConsenSys/very-deep-dive-on-ethereum-reading-list-f5b1122e5990#.d9mtt32ic" target="_blank" class="learnLink">Ethereum Reading List</a> for more in depth learning<br>
+  <a href="http://decypher.tv/series/ethereum-development" target="_blank" class="learnLink">Video Series</a> for Ethereum Ðapp Development (paid)<br>
+  <a href="https://academy.b9lab.com/" target="_blank" class="learnLink">Certified Online Course</a> for Ethereum Developer (paid)<br>
+  <a href="http://exosphe.re/#blockchain" target="_blank" class="learnLink">8 week immersive program</a> for Blockchain App Dev (paid)<br>
+  <br>
+  <h4>News and communities</h4>
+  <a href="https://blog.ethereum.org/" target="_blank" class="learnLink">Official Ethereum blog</a> by the Ethereum Foundation<br>
+  <a href="https://media.consensys.net/" target="_blank" class="learnLink">ConsenSys Blog</a> Blog of the Ethereum powerhouse <a href="https://consensys.net/" target="_blank" class="learnLink">ConsenSys</a><br>
+  <a href="https://www.ethnews.com/" target="_blank" class="learnLink">ETHNews</a> News Website<br>
+  <a href="https://cointelegraph.com/" target="_blank" class="learnLink">Cointelegraph</a> News Website<br>
+  <a href="http://www.coindesk.com/?s=ethereum#" target="_blank" class="learnLink">Coindesk</a> News Website<br>
+  <a href="http://www.weekinethereum.com/" target="_blank" class="learnLink">The week in Ethereum</a> newsletter by Evan Van Ness<br>
+  <a href="https://www.reddit.com/r/ethereum/" target="_blank" class="learnLink">Ethereum subreddit</a> daily news, development Most news about Ethereum can be found here daily<br>
+  <a href="https://www.reddit.com/r/ethtrader/" target="_blank" class="learnLink">Ethtrader subreddit</a> ether price and trading discussions<br>
+  <br>
+  <h4>Exchanges</h4>
+  <a href="https://www.ethnews.com/" target="_blank" class="learnLink">ETHNews</a> News Website<br>
+  <a href="https://www.coinbase.com/" target="_blank" class="learnLink">Coinbase</a> Exchange based in the US<br>
+  <a href="https://www.kraken.com/" target="_blank" class="learnLink">Kraken</a> Exchange base in the US<br>
+  <a href="https://www.bitfinex.com/" target="_blank" class="learnLink">Bitfinex</a> Exchange based in China<br>
+  <a href="https://yunbi.com/" target="_blank" class="learnLink">Yunbi</a> Exchange based in China<br>
+  <a href="http://www.poloniex.com/" target="_blank" class="learnLink">Poloniex</a> Cryptocurrency only Exchange<br>
+  <a href="https://www.bittrex.com/" target="_blank" class="learnLink">Bittrex</a> Cryptocurrency only Exchange<br>
+  <a href="https://www.ethnews.com/" target="_blank" class="learnLink">ETHNews</a> News Website<br>
+  <br>
 </div>
 </template>
 
