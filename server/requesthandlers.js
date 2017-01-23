2dcab1514ddf3af0c1fb0a1c7b88da6a0f1480ab
Michael Ilg <michael@ilg.ac>
2017-01-23 00:09:04 -0800

    Implement passport

--- server/requesthandlers.js
+++ server/requesthandlers.js
@@ -1,6 +1,11 @@
+var authRouter = require('express').Router()
+var jwt = require('jwt-simple')
+// var bcrypt = require('bcryptjs')
+var moment = require('moment')
 var db = require('./db.js')
 
 let sendAllDapps = (req, res) => {
+  console.log('sendingAllDapps')
   db.Dapp.find().then(dapps => res.send(dapps))
 }
 
@@ -31,7 +36,18 @@ let saveDapp = (req, res) => {
   })
 }
 
+let login = (req, res) => {
+  let payload = { id: 1, exp: moment().add(30, 'days').unix() }
+  let token = jwt.encode(payload, process.env.jwtSecret)
+  if (req.body.username === process.env.username && req.body.password === process.env.password) {
+    res.json({ token: token })
+  } else {
+    res.status(401).send('auth error')
+  }
+}
+
 module.exports = {
   sendAllDapps,
-  saveDapp
+  saveDapp,
+  login
 }
