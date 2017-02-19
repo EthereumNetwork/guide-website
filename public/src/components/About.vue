0aa02784b8f94c0a502d41c0367cdb615d3c8485
Michael Ilg <michael@ilg.ac>
2017-02-19 09:58:12 -0800

    Update about page

--- public/src/components/About.vue
+++ public/src/components/About.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <h1>Ethereum Network</h1>
-    <p>Ethereum Network is a guide to help the commnunity to better understand, use and explore <a href="https://ethereum.org/" target="_blank">Ethereum</a>, the decentralized smart contracts platform.</p>
-    <p> <a href="https://ethereum.org/" target="_blank">Ethereum</a>, the decentralized smart contracts platform.</p>
-
-
-    <h1>Team</h1>
-  </div>
+      <h1>Goal</h1>
+      <p>Ethereum Network is a guide to help the community to better understand, use and explore <a href="https://ethereum.org/" target="_blank">Ethereum</a>, the decentralized smart contracts platform.</p>
+      <p>The Learn page is a curated, up-to-date list of links to show users and developers first steps into Ethereum.<br>
+      <p>The Project list gives an overview of all projects using Ethereum in some way, searchable by their keywords, technologies, developers and other criteria. It is a tools to find ideas and people with the same interests to form connections and work together, rather than a list to brag how many projects there are on Ethereum. So will also find abandoned projects and the ones who never made it past the idea stage, just check the details by clicking on the title.<br>To share your findings, you can link directly to the project details or a search query like <router-link to="/projects?q=ether">https://ethereum.network/projects?q=ether</router-link></p>
+      <p>With the Explorer you can analyze the Ethereum blockchain in detail, see transactions coming in in real time, verify smart contract code and view statistics.<br>In our blog we report from Ethereum meetups around the world, let us know if you want our readers to get to know your group, too.<br>or about any feature you would like to see here next.</p>
+      <br>
+      <h1>Team</h1>
+      <h2>Michael Ilg</h2>
+      <v-row>
+      <img src="../assets/milg.jpg">
+      <v-col xs12 sm6>
+      <p> started out with programming on the <a href="http://bravenewcoin.com/news/lightlist-io-makes-lighthouse-seamless" target="_blank">Bitcoin blockchain</a> before moving on to Ethereum to decentralize all the things.</p>
+      <a href="https://github.com/misterigl" target="_blank"> <i class="fa fa-github contact-link"> </i></a>
+      <a href="https://twitter.com/misterigl" target="_blank"> <i class="fa fa-twitter contact-link"> </a>
+      <a href="https://www.reddit.com/user/misterigl/" target="_blank"> <i class="fa fa-reddit contact-link"></a>
+    </v-col>
+  </v-row>
+</div>
 </template>
 
 <script>
