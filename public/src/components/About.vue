c0e009ee984a149759fa09c7e85e3180c4c29252
Michael Ilg <michael@ilg.ac>
2017-02-18 21:46:57 -0800

    Add about text

--- public/src/components/About.vue
+++ public/src/components/About.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p>some about text</p>
+    <h1>Ethereum Network</h1>
+    <p>Ethereum Network is a guide to help the commnunity to better understand, use and explore <a href="https://ethereum.org/" target="_blank">Ethereum</a>, the decentralized smart contracts platform.</p>
+    <p> <a href="https://ethereum.org/" target="_blank">Ethereum</a>, the decentralized smart contracts platform.</p>
+
+
+    <h1>Team</h1>
   </div>
 </template>
 
