23bfb836a38c33e9217a905081abf22f8a8c8560
Michael <your_email@example.com>
2017-04-04 06:59:02 +0200

    Integrate i18n mangager

---
+++ public/src/i18n.js
@@ -0,0 +1,18 @@
+import en from './static/en.js'
+
+const config = {
+  defaultCode: 'en-GB',
+  languages: [
+    {
+      name: 'English',
+      code: 'en-GB',
+      urlPrefix: 'en',
+      translationKey: 'en'
+    }
+  ],
+  translations: {
+    en: en
+  }
+}
+
+export default config
--- public/src/main.js
+++ public/src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import store from './store.js'
 import router from './router.js'
+import config from './i18n.js'
+import VueI18nManager from 'vue-i18n-manager'
 import VueCookie from 'vue-cookie'
 import VueWebsocket from 'vue-websocket'
 
 import App from './components/App.vue'
 
 Vue.use(Vuetify)
+Vue.use(VueI18nManager, { store, config })
 Vue.use(VueCookie)
 Vue.use(VueWebsocket, '/', {'path': '/socket/socket.io'})
 
@@ -17,3 +20,5 @@ const app = new Vue({
   router,
   render: h => h(App)
 })
+
+Vue.initI18nManager()
