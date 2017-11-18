a8213695e3b3e7cf2f5a225c3e79cd1165b9d7ae
DmitryAlCh <dmi.al.ch@gmail.com>
2017-11-18 12:18:04 +0200

    Addres component table design updated

--- public/src/components/Address.vue
+++ public/src/components/Address.vue
@@ -4,22 +4,18 @@
     <VueQrcode class="qrcode" id="tdsfgdsfgs" :text="this.$route.params.id" :size="128"></VueQrcode>
     <p>I'm still building up an address-transaction database inlcluding the pending transactions. Until then, old transactions are coming from <a href="https://etherscan.io/">Etherscan</a> (They are awesome), but new transactions should show up here in realtime...</p>
     <p>ETH balance: {{balance/1e18}} Ether (${{Math.round(balance/1e16*price.USD)/100}})</p>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="header in headers" v-text="header"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <template v-for="tx in transactionList">
-          <tr>
-            <td><router-link :to="'/address/' + tx.from">{{tx.from.slice(0,10)}}...</router-link></td>
-            <td><router-link :to="'/address/' + tx.to">{{tx.to.slice(0,10)}}...</router-link></td>
-            <td><router-link :to="'/tx/' + tx.hash">{{Math.round(tx.value/1e10)/1e8}} Ether</router-link> (${{Math.round(tx.value/1e16*price.USD)/100}})</td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
+    <v-data-table
+      v-bind:headers = "headers"
+      v-bind:items = "transactionList"
+      class="elevation-1"
+    >
+    <template slot="items" slot-scope="props">
+      <td><router-link :to="'/address/' + props.item.from">{{props.item.from.slice(0,10)}}...</router-link></td>
+      <td><router-link :to="'/address/' + props.item.to">{{props.item.to.slice(0,10)}}...</router-link></td>
+      <td><router-link :to="'/tx/' + props.item.hash">{{Math.round(props.item.value/1e10)/1e8}} Ether</router-link> (${{Math.round(props.item.value/1e16*price.USD)/100}})</td>
+    </template>
+    </v-data-table>
+  
     <div class="comments">
       <VueDisqus shortname="ethereumnetwork" :identifier="$route.path" :url="'https://ethereum.network' + $route.path"></VueDisqus>
     </div>
@@ -36,7 +32,11 @@ export default {
     return {
       msg: 'the network explorer is in the works...',
       balance: -0,
-      headers: ['from', 'to', 'value'],
+      headers: [
+        {text: 'From', value: 'from', sortable: false, align: 'left'},
+        {text: 'To', value: 'to', sortable: false, align: 'left'},
+        {text: 'Value', value: 'value', sortable: true, align: 'left'},
+      ],
       transactionList: []
     }
   },
@@ -56,6 +56,7 @@ export default {
     this.$store.state.dsClient.event.subscribe('pending/' + this.$route.params.id, (txData) => {
       this.transactionList.unshift(txData)
       this.transactionList = this.transactionList.slice(0, 50)
+      
     })
   },
   components: {
