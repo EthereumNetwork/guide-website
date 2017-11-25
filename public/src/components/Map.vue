3a9c6a88d3371b1158326eee5e3da5df4536e0aa
DmitryAlCh <dmi.al.ch@gmail.com>
2017-11-25 22:38:40 +0200

    Added radio buttons to select which markers to select

--- public/src/components/Map.vue
+++ public/src/components/Map.vue
@@ -1,26 +1,41 @@
 <template>
-  <v-container grid-list-md>
+  <v-container fluid grid-list>
     <v-layout row wrap>
       <v-card-text>
       Map coming soon, with nodes from <a href="https://www.ethernodes.org" target="_blank"> ethernodes.org</a>, meetups from <a href="https://www.meetup.com/topics/ethereum/" target="_blank">Meetup.com</a>, as well as ether-cash trades from <a href="https://dether.io/" target="_blank">Dether</a> and <a href="https://localethereum.com/" target="_blank">localethereum</a>
       </v-card-text>
     </v-layout>
+    
     <v-layout row wrap>
-      <v-card-media>
-        <gmap-map
-          :center = "center"
-          :zoom = 3
-          style="width:600px; height: 400px;"
-        >
-            <gmap-marker
-              :key="index"
-              v-for="(m, index) in markers"
-              :position="m.position"
-              @click="center=m.position">
-            </gmap-marker>
-        </gmap-map>
-      </v-card-media>
-      </v-layout>
+      <v-flex xs12 sm6>
+        <v-card>
+        <v-card-title primary-title>
+          <div class="Headline">Interactive map of nodes, meetups etc.</div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn-toggle v-model="toggle_exclusive">
+            <v-btn flat color="primary" @click="nodesMap">Nodes</v-btn>
+            <v-btn flat color="default" @click="meetupMap">MeetUps</v-btn>
+            <v-btn flat color="default" @click="etcMap">Etc</v-btn>
+          </v-btn-toggle>
+        </v-card-actions>
+        <v-card-media class="ma-1 text-xs-center" id="mapHolder">
+          <gmap-map id = "gmap"
+            :center = "center"
+            :zoom = 3
+            style="width: 500px; height: 300px;"
+          >
+              <gmap-marker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                @click="center=m.position">
+              </gmap-marker>
+          </gmap-map>
+        </v-card-media>
+        </v-card>
+      </v-flex>  
+    </v-layout>
   </v-container>
   
 </template>
@@ -30,6 +45,7 @@
     name: 'Map',
     data () {
       return {
+        toggle_exclusive: 0,
         name: 'map',
         // dialog: false,
         center: {lat: 10.123, lng: 10.44321},
@@ -54,6 +70,50 @@
         }]
       }
     },
+    methods: {
+      nodesMap: function(event){
+        // A fetch('api/maps/nodes') should be here
+
+        this.markers = [{
+          position: {lat: 54.0, lng: 21.0}
+        }, {
+          position: {lat: 54.0, lng: 22.0}
+        }, {
+          position: {lat: 54.0, lng: 23.0}
+        }, {
+          position: {lat: 54.0, lng: 24.0}
+        }]
+        // change data source
+        // change data vizualization
+        // set selected active
+      },
+      meetupMap: function(event){
+        // A fetch('api/maps/meetups') should be here
+        this.markers = [{
+          position: {lat: 50.0, lng: 21.0}
+        }, {
+          position: {lat: 51.2, lng: 22.2}
+        }, {
+          position: {lat: 52.2, lng: 23.2}
+        }, {
+          position: {lat: 53.2, lng: 24.2}
+        }]
+      },
+      etcMap: function(event){
+        // A fetch('api/maps/somethingElse') should be here
+        this.markers = [{
+          position: {lat: 55.0, lng: 31.0}
+        }, {
+          position: {lat: 54.2, lng: 32.2}
+        }, {
+          position: {lat: 53.2, lng: 33.2}
+        }, {
+          position: {lat: 52.2, lng: 34.2}
+        }]
+      }
+       
+    },
+
     beforeCreate(){
       
     },
@@ -81,7 +141,20 @@
       setCenter().then((centerObj) => {
         this.center = centerObj;
       });
+
+     
                    
+    },
+    mounted () {
+      // var mapHolder = document.getElementById('mapHolder');
+      // var mapHheight = mapHolder.offsetHeight;
+      // var mapHwidth = mapHolder.offsetWidth;
+      // console.log('Parent dimensions: ');
+      // console.log(mapHheight, mapHwidth);
+      // var map = document.getElementById('gmap');
+      // map.style.height = mapHheight - 20;
+      // map.style.width = mapHwidth - 20;
+      
     },
     
   }
@@ -91,8 +164,9 @@
   .wechat-image {
     max-width: 290px;
   }
-  gmap-map {
+  #gmap {
     width: 500px;
     height: 300px;
   }
+  
 </style>
